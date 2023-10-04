d0b3285f198b2fcc56a05a6be5f64682843a4b68
Nikolay Dederer <cmrddrr@gmail.com>
2023-10-04 16:08:37 +0300

    task64 finished

---
+++ task64/Program.cs
@@ -0,0 +1,24 @@
+﻿int GetNumber()
+{
+    Console.WriteLine("Введите число:");
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
+
+int Seq (int num)
+{
+    
+    if (num!=1)
+    {
+        Console.Write($"{num}, ");
+        return Seq(num-1);
+    }
+    else
+    {
+        Console.Write($"{num}.");
+        return num;
+    }   
+}
+
+int n = GetNumber();
+Seq(n);
