1c62e1f531ea518039b59ffb8685f507da774402
Nikolay Dederer <cmrddrr@gmail.com>
2023-10-04 16:41:16 +0300

    task66 finished

---
+++ task66/Program.cs
@@ -0,0 +1,32 @@
+﻿int GetNumber(string mess)
+{
+    Console.WriteLine($"Введите число {mess}:");
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
+
+int Sum (int m, int n, int res=0)
+{
+   
+    if (m>=n)
+    {
+        res+=m;
+        return Sum(m-1, n, res);
+    }
+    else
+    {
+        
+        return res;
+    }   
+}
+
+int m=GetNumber("M");
+int n=GetNumber("N");
+if (m<n)
+    {
+        int temp=n;
+        n=m;
+        m=temp;
+    }
+int res=Sum(m, n);
+Console.WriteLine($"Сумма равна {res}.");
